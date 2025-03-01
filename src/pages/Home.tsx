efff8b883389e4ad2857fa16b06431e3750b6b32
unknown <edosegheosamagbe@gmail.com>
2025-03-01 13:19:47 +0000

    Setting up router and tailwind

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1> Content </h1>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
