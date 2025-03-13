a985864bcd6c26e85c50688b7c5ce3f459a03c20
unknown <edosegheosamagbe@gmail.com>
2025-03-13 11:24:11 +0000

    User context fully configured

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -5,15 +5,17 @@ interface AuthContextType {
   isSignedIn: boolean;
   setIsSignedIn: Function;
   userToken?: string;
+  userData: any;
+  setUserData: Function;
 }
 
 //CONTEXT SETUP
 const GlobalUserContext = createContext<AuthContextType | null>(null);
 
 export const useGlobalUserContext = () => {
-  const {isSignedIn, setIsSignedIn, userToken}: any = useContext(GlobalUserContext);
+  const {isSignedIn, setIsSignedIn, userToken, userData, setUserData}: any = useContext(GlobalUserContext);
 
-  return {isSignedIn, setIsSignedIn, userToken}
+  return {isSignedIn, setIsSignedIn, userToken, userData, setUserData}
 }
 
 
@@ -21,32 +23,38 @@ export const useGlobalUserContext = () => {
 
 //NEEDED CONTEXT VALUES
 const storedToken = localStorage.getItem('pmw_token');
+const storedUser = localStorage.getItem("pmw_user");
 if (!storedToken) {
   console.error("No user is logged in");
 }
 
 let userToken: string | undefined;
+let user: any
 
-if(storedToken) {
+if(storedToken && storedUser) {
   try {
     userToken = JSON.parse(storedToken);
+    user = JSON.parse(storedUser);
   } catch (error) {
     console.error("Failed to parse token:", error);
     userToken = undefined
+    user = undefined
   }
 } else {
   console.error("No user is logged in");
   userToken = undefined;
+  user = undefined
 }
 
 const UserContext = ({ children }: any) => {
   const [isSignedIn, setIsSignedIn] = useState<boolean>(
     userToken ? true : false
   );
+  const [userData, setUserData] = useState<any>(user ? user : undefined)
 
   return (
     <GlobalUserContext.Provider
-      value={{ isSignedIn, setIsSignedIn, userToken }}
+      value={{ isSignedIn, setIsSignedIn, userToken, userData, setUserData }}
     >
       {children}
     </GlobalUserContext.Provider>
